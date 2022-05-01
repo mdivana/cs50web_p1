c97d93d2a11b274ebe6fd0841ec2b9000cb00e2b
Nika Mdivani <nikamdivani@icloud.com>
2022-05-01 23:55:17 +0400

    added search function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
+from audioop import reverse
 from curses import update_lines_cols
+from multiprocessing.sharedctypes import Value
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from markdown2 import Markdown
 
@@ -18,4 +21,20 @@ def entrypage(request, entry):
         return render(request, "encyclopedia/entrypage.html", {
             "entry": Markdown().convert(page),
             "entrytitle": entry,
+        })
+
+def search(request):
+    searchres = request.GET.get('q')
+    if(util.get_entry(searchres) is not None):
+        return HttpResponseRedirect(reverse('entry', kwargs = {'entry': searchres }))
+    else:
+        entrylist = []
+        for entry in util.list_entries():
+            if searchres.lower() in entry.lower():
+                entrylist.append(entry)
+
+        return render(request, "encyclopedia?index.html", {
+            "entries": entrylist,
+            "search": True,
+            "value": searchres
         })
